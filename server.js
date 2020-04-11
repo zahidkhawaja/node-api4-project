cbd4431cc684cb891a4be65bea26021360c1130d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-11 17:37:55 -0400

    Implement web sockets for all messages

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const { Server } = require('ws');
 const express = require('express');
-var cors = require('cors')
+var cors = require('cors');
 const userRouter = require("./users/userRouter");
+const User = require("./users/userDb");
 
 const server = express();
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(User.get());
+  });
+}, 1000);
 
 server.use(cors());
 server.use(logger);
